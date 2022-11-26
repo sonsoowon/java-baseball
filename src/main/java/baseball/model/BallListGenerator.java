90c59d6898c2f7c69b3e09371991c6d50f75c738
sonsoowon <yuwoo1018@gmail.com>
2022-11-26 15:05:30 +0900

    feat(BallListGenerator): 플레이어의 숫자 리스트를 객체 리스트로 변환

---
+++ src/main/java/baseball/model/BallListGenerator.java
@@ -0,0 +1,16 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BallListGenerator {
+
+    public static List<Ball> generateBallsOf(List<Integer> numbers) {
+        List<Ball> balls = new ArrayList<>();
+        for (int index = 0; index < numbers.size(); index++) {
+            Ball ball = new Ball(index, numbers.get(index));
+            balls.add(ball);
+        }
+        return balls;
+    }
+}
---
+++ src/test/java/baseball/model/BallListGeneratorTest.java
@@ -0,0 +1,24 @@
+package baseball.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BallListGeneratorTest {
+    @DisplayName("플레이어의 숫자 리스트를 볼 객체 리스트로 변환한다")
+    @Test
+    void generateBallListTest() {
+        List<Ball> actual = BallListGenerator.generateBallsOf(List.of(1, 2, 3));
+        List<Ball> expected = List.of(
+                new Ball(0, 1),
+                new Ball(1, 2),
+                new Ball(2, 3)
+        );
+
+        assertThat(actual).isEqualTo(expected);
+
+    }
+}
