503f1780ccd21a6ce6a07396ee26452e3bc64e8d
sonsoowon <yuwoo1018@gmail.com>
2022-11-26 13:54:39 +0900

    feat(InputView): 게임 재시작/종료 입력

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,6 @@
 package baseball.view;
 
+import baseball.model.GameCommandValidator;
 import baseball.model.PredictionValidator;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -10,11 +11,19 @@ public enum InputView {
     INSTANCE;
 
     private static final String PREDICTS_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GAME_COMMAND_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static List<Integer> readPrediction() throws IllegalArgumentException {
         System.out.println(PREDICTS_MESSAGE);
         String input = Console.readLine();
         return PredictionValidator.getValidInput(input);
+    }
+
+    public static String readGameCommand() throws IllegalArgumentException {
+        System.out.println(GAME_COMMAND_MESSAGE);
+        String input = Console.readLine();
+        GameCommandValidator.validate(input);
+        return input;
     }
 
 }
